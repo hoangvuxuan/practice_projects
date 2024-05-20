494fa66ab9a8c17dfc3578967c9441b2d5e582b0
xuan hoang <hoang24k69@gmail.com>
2024-05-21 02:28:31 +0700

    craw_data: create interface

---
+++ craw_data_basic/craw_data_basic/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using craw_data_basic.all_class;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace craw_data_basic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Menu_Tree_Item> tree_Items;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tree_Items = new ObservableCollection<Menu_Tree_Item>();
+            Menu_Tree_Item t1 = new Menu_Tree_Item()
+            {
+                Name = "Foo",
+                Url = "Bar",
+                Items = new ObservableCollection<Menu_Tree_Item>()
+                {
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                }
+            };
+
+            Menu_Tree_Item t2 = new Menu_Tree_Item()
+            {
+                Name = "Foo",
+                Url = "Bar",
+                Items = new ObservableCollection<Menu_Tree_Item>()
+                {
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                }
+            };
+
+            add_item_to_treeview(tree_Items, t1);
+            add_item_to_treeview(tree_Items, t2);
+
+            tree_main.ItemsSource = tree_Items;
+        }
+
+        void add_item_to_treeview(ObservableCollection<Menu_Tree_Item> root, Menu_Tree_Item node)
+        {
+            root.Add(node);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Menu_Tree_Item t3 = new Menu_Tree_Item()
+            {
+                Name = "Foo",
+                Url = "Bar",
+                Items = new ObservableCollection<Menu_Tree_Item>()
+                {
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                    new Menu_Tree_Item() {Name= "Foo", Url = "Bar"},
+                }
+            };
+
+            add_item_to_treeview(tree_Items[1].Items, t3);
+        }
+    }
+}
---
+++ craw_data_basic/craw_data_basic/all_class/Menu_Tree_Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craw_data_basic.all_class
+{
+    internal class Menu_Tree_Item
+    {
+        private string name;
+        private string url;
+        private ObservableCollection<Menu_Tree_Item> items;
+
+        public Menu_Tree_Item()
+        {
+            this.items = new ObservableCollection<Menu_Tree_Item>();
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Url { get => url; set => url = value; }
+        public ObservableCollection<Menu_Tree_Item> Items { get => items; set => items = value; }
+    }
+}
